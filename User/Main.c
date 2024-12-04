28741051cfb08e291bda6de6dc89e81c88c8e3f8
unknown <albthierryalb@gmail.com>
2024-12-04 20:39:47 +0100

    before new letter

--- User/Main.c
+++ User/Main.c
@@ -2,12 +2,15 @@
 
 To do 
 
+
+ajouter le ble
+ajouter si pas de message recu en 10 min afficher --
+print force du signal et temp hum ext
 changer police mar mer jeu ven sam dim janv	....
-affichr des -- quand le capteur dehors n'a rien envoyer
 coder min max
 ajouter dessin météo
 ajouter capteur I2C
-ajouter si pas de message recu en 10 min afficher --
+
 
 
 *******************************************************************************/
@@ -18,11 +21,13 @@ ajouter si pas de message recu en 10 min afficher --
 #include "Screen.h"
 #include "BlueNRG_Interface_Lib.h"
 #include "SHT4.h"
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
+#include "hal_radio.h"
+#include "osal.h"
+#include "fifo.h"
+#include "vtimer.h"
+#include "clock.h"
+
+
 uint8_t validate_press;
 int value_press;
 uint8_t minute_set;
@@ -31,9 +36,68 @@ uint8_t day_set;
 uint8_t day_week_set;
 uint8_t mouth_set;
 
+#define BLE_ADV_ACCESS_ADDRESS  (uint32_t)(0x8E89BED6)
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* Blue FIFO */
+circular_fifo_t blueRec_fifo;
+uint8_t blueRec_buffer[MAX_PACKET_LENGTH*2+MAX_PACKET_LENGTH];
+
+
+uint8_t receivedData[MAX_PACKET_LENGTH];
+
+uint32_t delay =1000;
+uint8_t channel = 39;                    
+uint32_t timeOut = 100000;
+int32_t rssi_val = 0;
+uint32_t timestamp = 0;
+
+
+uint8_t nullfunction(ActionPacket* p,  ActionPacket* next)
+{
+  return TRUE;
+}
+
+
+uint8_t RxCallback(ActionPacket* p, ActionPacket* next)
+{
+  if( (p->status & IRQ_DONE) != 0) {
+    
+    /* received a packet */
+    if( (p->status & BIT_TX_MODE) == 0) {
+      
+      if((p->status & IRQ_RCV_OK) != 0) {
+        /* Check if there is enough space in the FIFO to store the incoming data */
+        if(MAX_PACKET_LENGTH*2 - fifo_size(&blueRec_fifo) > (p->data[1]+ 4 + 4 + 4)){
+          fifo_put_var_len_item(&blueRec_fifo, p->data[1]+2, p->data);
+          fifo_put(&blueRec_fifo, 4, (uint8_t*)(&p->rssi));
+          fifo_put(&blueRec_fifo, 4, (uint8_t*)(&p->timestamp_receive));
+        }
+          HAL_RADIO_ReceivePacket(channel, delay, receivedData, timeOut, RxCallback);
+      }
+      else if((p->status & IRQ_TIMEOUT) != 0) {
+        HAL_RADIO_ReceivePacket(channel,delay, receivedData, timeOut, RxCallback);
+      }
+      else if((p->status & IRQ_CRC_ERR) != 0) {
+        HAL_RADIO_ReceivePacket(channel, delay, receivedData, timeOut, RxCallback);
+      }
+    }
+    
+    /* Transmit complete */ 
+    else {
+    }
+  }
+  return TRUE;
+}
+
 int main(void)
 {
+  uint8_t packet[MAX_PACKET_LENGTH];
+	HAL_VTIMER_InitType VTIMER_InitStruct = {HS_STARTUP_TIME, INITIAL_CALIBRATION, CALIBRATION_INTERVAL};
 
+  uint16_t length;
   /* System initialization function */
   SystemInit();
   
@@ -58,7 +122,7 @@ int main(void)
 	I2C_Read(0x10, 0, 1, ret, 2);*/
 	
 	Screen_init();
-	Screen_set_up();
+	//Screen_set_up();
 	float temp,hum;
 	while(1) 
 	{
